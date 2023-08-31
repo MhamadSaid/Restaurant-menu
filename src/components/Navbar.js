8e9288b22417badd453406160e739a003e7ab4d9
MhamadSaid <MhamadSaid0000@gmail.com>
2023-08-31 19:01:15 +0300

    edit nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,36 +17,32 @@ function Navbar() {
       <div className="leftSide" id={openLinks ? "open" : "close"}>
         <img src={Logo} alt="Logo" />
         <div className="hiddenLinks">
-          <Link to="/" smooth={true}>
-            {" "}
-            Home{" "}
+          <Link to="home" smooth={true}>
+            Home
           </Link>
-          <Link to="/menu" smooth={true}>
-            {" "}
-            Menu{" "}
+          <Link to="about" smooth={true}>
+            About
           </Link>
-          <Link to="/about" smooth={true}>
-            {" "}
-            About{" "}
+          <Link to="menu" smooth={true}>
+            Menu
           </Link>
-          <Link to="/contact" smooth={true}>
-            {" "}
-            Contact{" "}
+          <Link to="contact" smooth={true}>
+            Contact
           </Link>
         </div>
       </div>
       <div className="rightSide">
-        <div className="w-full bg-black/20 h-[70px] backdrop-blur-2xl rounded-full max-w-[350px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
-          <Link to="/" smooth={true}>
+        <div>
+          <Link to="home" smooth={true}>
             Home
           </Link>
-          <Link to="/about" smooth={true}>
+          <Link to="about" smooth={true}>
             About
           </Link>
-          <Link to="/menu" smooth={true}>
+          <Link to="menu" smooth={true}>
             Menu
           </Link>
-          <Link to="/contact" smooth={true}>
+          <Link to="contact" smooth={true}>
             Contact
           </Link>
         </div>
