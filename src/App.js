81b905f01281ac778e6724810c7f1cb75ff5d202
MhamadSaid <MhamadSaid0000@gmail.com>
2023-09-04 19:02:45 +0300

    edit rountig

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import Routes
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Home />
-        <About />
-        <Menu />
-        <Contact />
-        <Footer />
-      </Router>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
