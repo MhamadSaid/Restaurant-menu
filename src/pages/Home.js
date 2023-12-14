b35480af4a80537c6765411cb58f350a1315f563
MhamadSaid <MhamadSaid0000@gmail.com>
2023-12-14 17:47:12 +0200

    edit home:responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BannerImage from "../assets/pizza23.jpg";
+import BannerImage from "../assets/Home.jpg";
 import "../styles/Home.css";
 
 function Home() {
